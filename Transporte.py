46f47c0018d6d7c96d5164c7911fd7aa8cc30495
JoSantana0005 <joseasantana05@gmail.com>
2025-03-24 13:02:09 -0400

    Colocando la solicitud de la oferta y la demanda del problema

--- Transporte.py
+++ Transporte.py
@@ -8,3 +8,10 @@ def main():
 
     ciudades_destino = input("Ingrese las ciudades de destino (separadas por comas): ").split(',')
     ciudades_destino = [ciudad.strip() for ciudad in ciudades_destino]
+
+    # Solicitar la oferta y demanda
+    oferta = list(map(int, input("Ingrese la oferta de cada ciudad de origen (separadas por comas): ").split(',')))
+
+    demanda = list(map(int, input("Ingrese la demanda de cada ciudad de destino (separadas por comas): ").split(',')))
+
+    

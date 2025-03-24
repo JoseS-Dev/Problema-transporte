bc12de3e497e18fe6e13f4830c8af9268651076a
JoSantana0005 <joseasantana05@gmail.com>
2025-03-24 13:00:38 -0400

    Colocando la solicitud de las ciudades de origen y destino

--- Transporte.py
+++ Transporte.py
@@ -2,4 +2,9 @@
 import pulp #Libreria para resolver problemas de programación lineal
 
 def main():
-    
+    # Solicitar al usuario las ciudades de origen y destino
+    ciudades_origen = input("Ingrese las ciudades de origen (separadas por comas): ").split(',')
+    ciudades_origen = [ciudad.strip() for ciudad in ciudades_origen]
+
+    ciudades_destino = input("Ingrese las ciudades de destino (separadas por comas): ").split(',')
+    ciudades_destino = [ciudad.strip() for ciudad in ciudades_destino]
